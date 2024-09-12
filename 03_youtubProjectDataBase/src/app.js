abe38ce812bd0b3da7297c0569943945033d623d
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-09-12 10:55:17 +0530

    Handle file by using cloudinary

--- 03_youtubProjectDataBase/src/app.js
+++ 03_youtubProjectDataBase/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public"))
 
+app.use(cookieParser());
 
 // import routes
 import  healthcheckRouter  from "./routes/healthcheck.route.js";
--- 03_youtubProjectDataBase/src/routes/healthcheck.route.js
+++ 03_youtubProjectDataBase/src/routes/healthcheck.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import {healthcheck} from "../controllers/healthcheck.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
